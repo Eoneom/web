9efb3440234e5ecf75defa12766407ad95d047bc
Valentin <6004961+Kroustille@users.noreply.github.com>
2023-02-08 20:40:14 +0100

    feat: add sync call to get initial data

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface SyncResponse {
+  city: {
+    plastic: number
+    mushroom: number
+  }
 }
 
-export default App;
+const sync = async (token: string) : Promise<SyncResponse> => {
+  const res = await fetch('http://localhost:3000/sync', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ token })
+  })
+
+  const data = await res.json()
+  return data
+}
+
+const App = () => {
+  const token = 'Kroustille'
+  const [mushroom, setMushroom] = useState(0)
+  const [plastic, setPlastic] = useState(0)
+
+  useEffect(() => { sync(token).then(data => {
+    console.log(data)
+    setMushroom(data.city.mushroom)
+    setPlastic(data.city.plastic)
+  }) }, [])
+
+  return (
+    <main>
+      <header>
+        <p>
+          Plastique: {plastic}
+        </p>
+        <p>
+          Champignon: {mushroom}
+        </p>
+      </header>
+    </main>
+  )
+}
+
+export default App
