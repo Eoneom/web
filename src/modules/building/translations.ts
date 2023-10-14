ac4f70beaafb8559a59536ec3352433289047700
Kroustille <6004961+Kroustille@users.noreply.github.com>
2023-10-14 21:53:02 +0200

    feat: add warehouse buildings and display city limit

--- src/modules/building/translations.ts
+++ src/modules/building/translations.ts
@@ -2,7 +2,9 @@ import { BuildingCode } from '@kroust/swarm-client'
 
 export const BuildingTranslations: Record<BuildingCode, { name: string }> = {
   [BuildingCode.MUSHROOM_FARM]: { name: 'Ferme à champignons' },
+  [BuildingCode.MUSHROOM_WAREHOUSE]: { name: 'Entrepôt de champignon' },
   [BuildingCode.RECYCLING_PLANT]: { name: 'Centre de recyclage' },
+  [BuildingCode.PLASTIC_WAREHOUSE]: { name: 'Entrepôt de plastique' },
   [BuildingCode.RESEARCH_LAB]: { name: 'Laboratoire de recherche' },
   [BuildingCode.UNIVERSITY]: { name: 'Université' }
 }
--- src/ui/nav/bar.tsx
+++ src/ui/nav/bar.tsx
@@ -9,12 +9,15 @@ export const NavBar: React.FC = () => {
     return null
   }
 
+  const plasticClassName = selectedCity.plastic === selectedCity.warehouses_capacity.plastic ? 'danger' : 'success'
+  const mushroomClassName = selectedCity.mushroom === selectedCity.warehouses_capacity.mushroom ? 'danger' : 'success'
+
   return <nav id='nav-bar'>
     <h4>Swarm</h4>
     <h1><Link to={'/'}>{selectedCity.name}</Link></h1>
     <ul>
-      <li>Plastique: {transformDecimals(selectedCity.plastic)} (~{Math.round(selectedCity.earnings_per_second.plastic)}/s)</li>
-      <li>Champignon: {transformDecimals(selectedCity.mushroom)} (~{Math.round(selectedCity.earnings_per_second.mushroom)}/s)</li>
+      <li className={plasticClassName}>Plastique: {transformDecimals(selectedCity.plastic)} (~{transformDecimals(Math.round(selectedCity.earnings_per_second.plastic*3600))}/h)</li>
+      <li className={mushroomClassName}>Champignon: {transformDecimals(selectedCity.mushroom)} (~{transformDecimals(Math.round(selectedCity.earnings_per_second.mushroom*3600))}/h)</li>
     </ul>
   </nav>
 }
